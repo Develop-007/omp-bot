727cd35ac18f12f1bd7c6422b45cf60fd8d17354
Develop-007 <52546506+Develop-007@users.noreply.github.com>
2021-10-17 13:58:07 +0300

    Update help message

--- internal/app/commands/cinema/seat/command_help.go
+++ internal/app/commands/cinema/seat/command_help.go
@@ -4,11 +4,14 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func (c *CinemaSeatCommander) Help(inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
-		"/help - help\n"+
-			"/list - list products",
-	)
+const helpMessage = "/help__cinema__seat — print list of commands\n" +
+	"/get__cinema__seat — get a entity\n" +
+	"/list__cinema__seat — get a list of your entity\n" +
+	"/delete__cinema__seat — delete an existing entity\n" +
+	"/new__cinema__seat — create a new entity\n" +
+	"/edit__cinema__seat — edit a entity"
 
+func (c *CinemaSeatCommander) Help(inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, helpMessage)
 	c.bot.Send(msg)
 }
